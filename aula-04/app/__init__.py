4fb4c91c38356ee47db9b1d753a9ff9dbada129c
Alan Lima da Silva Guerra <esalanguerra@gmail.com>
2024-10-09 20:49:56 -0300

    aula 4

---
+++ aula-04/app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app.controllers import default
+
+if __name__ == '__main__':
+    app.run(debug=True)
